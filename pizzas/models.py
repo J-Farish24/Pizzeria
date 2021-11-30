ebb4fab80a0c6b7a00122d4ba1081153c415fd53
Josh Farish <josh_farish1@baylor.edu>
2021-11-30 17:09:49 -0600

    Added topping model that has a foreign key of pizza

--- pizzas/models.py
+++ pizzas/models.py
@@ -8,4 +8,10 @@ class Pizza(models.Model):
 
     def __str__(self):
         return self.name
-        
+
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
