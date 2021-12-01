11aae41e73a98f8b717e329e84c3762e6f070992
Josh Farish <josh_farish1@baylor.edu>
2021-12-01 10:52:59 -0600

    Added Pizzas page for user to view list of pizzas in alpabetical order

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import Pizza
+
 # Create your views here.
 def index(request):
     #Home page for pizzeria website
     return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    pizzas = Pizza.objects.all().order_by('name')
+
+    context = {'pizzas': pizzas}
+
+    return render(request, 'pizzas/pizzas.html', context)
