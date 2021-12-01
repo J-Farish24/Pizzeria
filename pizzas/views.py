955da98cb0b682c7f91fe3d25b0edcf482518a9e
Josh Farish <josh_farish1@baylor.edu>
2021-12-01 12:16:46 -0600

    Added individual pizza page that displays the toppings on the pizza

--- pizzas/views.py
+++ pizzas/views.py
@@ -13,3 +13,10 @@ def pizzas(request):
     context = {'pizzas': pizzas}
 
     return render(request, 'pizzas/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('name')
+    context = {'pizza': pizza, 'toppings': toppings}
+
+    return render(request, 'pizzas/pizza.html', context)
