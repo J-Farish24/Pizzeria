df46d71361f7ef4d984d4f223101fe3103a78d82
Josh Farish <josh_farish1@baylor.edu>
2021-12-12 16:14:41 -0600

    Added pictures and sorted comments by date descending

--- pizzas/views.py
+++ pizzas/views.py
@@ -18,7 +18,7 @@ def pizzas(request):
 def pizza(request, pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
     toppings = pizza.topping_set.order_by('name')
-    comments= pizza.comment_set
+    comments= pizza.comment_set.order_by('-date_created', )
     context = {'pizza': pizza, 'toppings': toppings, 'comments': comments}
 
     return render(request, 'pizzas/pizza.html', context)
